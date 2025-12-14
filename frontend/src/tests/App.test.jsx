9b72e22dffdd93802230e224b48c617cbd8be73f
Aniket Dekate <aniketdekate1@gmail.com>
2025-12-14 14:27:19 +0530

    chore: remove obsolete App.test.jsx from CRA template

--- frontend/src/tests/App.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "../App.jsx";
-
-test("renders Hello World text", () => {
-  render(<App />);
-  const element = screen.getByText(/hello world/i);
-  expect(element).toBeInTheDocument();
-});
