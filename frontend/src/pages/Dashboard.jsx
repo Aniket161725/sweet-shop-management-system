d9318b05a66c171ce6d68903ee901e3679a4b882
Aniket Dekate <aniketdekate1@gmail.com>
2025-12-14 15:01:05 +0530

    feat(dashboard): implement minimal dashboard structure to satisfy tests (GREEN)
    - Added dashboard title
    - Added basic pagination buttons

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Sweet Shop Dashboard</h1>
+
+      {/* for TDD, minimal pagination */}
+      <button>Previous</button>
+      <button>Next</button>
+    </div>
+  );
+};
+
+export default Dashboard;
